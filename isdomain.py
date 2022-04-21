59c191ec6e1abbaf4b83d406305dcb9590a4092c
Sumiza <64884509+Sumiza@users.noreply.github.com>
2022-04-20 22:50:28 -0400

    Update isdomain.py

--- isdomain.py
+++ isdomain.py
@@ -1,4 +1,4 @@
 import re
-a = re.search('^^[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9][.][a-zA-Z]*$',"google.com")
+a = re.search('^[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9][.][a-zA-Z]*$',"google.com")
 if a:
     print(a.string)
