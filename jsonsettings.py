071da94c666bf5b1cda46714f612d684d4b180d1
Sumiza <64884509+Sumiza@users.noreply.github.com>
2022-05-18 02:09:18 -0400

    Update jsonsettings.py

--- jsonsettings.py
+++ jsonsettings.py
@@ -1,8 +1,8 @@
+
 """
     Json Settings Read / Writer
 """
-import json
-def settingsload(filename:str):
+def settingsload(filename:str='settings.json',allowempty:bool=False):
     """
         - Read settings file from json format and return it
         - Write in any settings that are blank with option to save to file
@@ -11,6 +11,7 @@ def settingsload(filename:str):
                 - string, int, float
                 - [list], {"dict":"value"}
     """
+    import json
     def tryjson(value): # write str to proper type (list,int...)
         try:
             value = json.loads(value)
@@ -26,14 +27,15 @@ def settingsload(filename:str):
         with open(filename, "r",encoding='utf-8') as file:
             settings = json.loads(file.read())
             change = False
-            for check in settings:
-                if settings[check] == "":
-                    settings[check] = tryjson(input(f"Please input value for {check}: "))
-                    change = True
-            if change:
-                answer = input("Do you want to save the changes to file? (y/n) ").casefold()
-                if answer == "y":
-                    settingssave()
+            if allowempty is False:
+                for check in settings:
+                    if settings[check] == "":
+                        settings[check] = tryjson(input(f"Please input value for {check}: "))
+                        change = True
+                if change:
+                    answer = input("Do you want to save the changes to file? (y/n) ").casefold()
+                    if answer == "y":
+                        settingssave()
             return settings
 
     except FileNotFoundError:
@@ -50,6 +52,6 @@ def settingsload(filename:str):
                 return settingsload(filename)
             value = tryjson(input("Please write the Value: "))
             settings[key] = value
-            print(json.dumps(settings,indent=4))
+            print(json.dumps(settings,indent=6))
 
-print(settingsload("settings.json"))
+# print(settingsload("settings.json",allowempty=True))
