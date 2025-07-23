8ee756e4dd03a1ef0e0e3c4bc4b6ac23d62d63e2
Sumiza <64884509+Sumiza@users.noreply.github.com>
2025-07-23 15:32:57 -0500

    Update dockerpod.sh

--- dockerpod.sh
+++ dockerpod.sh
@@ -1,108 +1,134 @@
-#!/bin/bash
+#!/bin/sh
 
-if [ -z "$1" ]; then
-    echo "Compose stack yml file needed (./run file.yml)"
+if [ $# -lt 2 ]; then
+    echo "Usage: ./run <compose-file.yml> [remove|clean|update|deploy [replicas|--all]]"
     exit 1
 fi
 
-filename="$1"
-name=$(echo "$filename" | cut -d '.' -f 1)
+FILENAME="$1"
+name=$(echo "$FILENAME" | cut -d '.' -f 1)
+DOCKER=$(command -v docker)
 
-removeall(){
-    nodes=$(/usr/bin/docker node ls --format "{{.Hostname}}")
+get_nodes(){
+    $DOCKER node ls --format "{{.Hostname}} {{.Status}} {{.Availability}}"
+}
+
+get_stack(){
+    $DOCKER stack ls --format "{{.Name}}"
+}
+
+remove_all_stacks(){
+    nodes=$(get_nodes | cut -d ' ' -f 1)
     for node in ${nodes}; do
-        docker stack rm "$filename" "$name"-"$node" 2> /dev/null
+        $DOCKER stack rm "$name"-"$node" 2> /dev/null
     done 
 }
 
-update(){
-    stacks=$(/usr/bin/docker stack ls --format "{{.Name}}")
-    nodes=$(/usr/bin/docker node ls --format "{{.Hostname}}")
-    for node in ${nodes};do
-        stackname="$name"-"$node"
-        if echo "$stacks" | grep -q "$stackname"; then
-            /usr/bin/docker stack deploy -d -c "$filename" "$name"-"$node" 
-        fi
-    done
-}
-
-cleanup(){
-    nodes=$(/usr/bin/docker node ls --format "{{.Hostname}} {{.Status}} {{.Availability}}" | grep -v "Ready Active" | cut -d ' ' -f 1)
+update_stack(){
+    stacks=$(get_stack)
+    nodes=$(get_nodes | cut -d ' ' -f 1)
     for node in ${nodes}; do
-        /usr/bin/docker stack rm "$name"-"$node" 2> /dev/null
-    done
-}
-
-deployone(){
-    /usr/bin/docker stack deploy -d -c "$filename" "$name"-"$1"
-    serviceID=$(/usr/bin/docker stack services "$name"-"$1" -q)
-    /usr/bin/docker service update -d --constraint-add node.hostname=="$1" "$serviceID"
-}
-
-deployglobal(){
-    nodes=$(/usr/bin/docker node ls --format "{{.Hostname}} {{.Status}} {{.Availability}}" | grep "Ready Active" | sort | cut -d ' ' -f 1)
-    for node in ${nodes};do
-            deployone "$node"
-    done
-}
-
-deployn(){
-    replicate=$1
-    stacks=$(/usr/bin/docker stack ls --format "{{.Name}}")
-    nodes=$(/usr/bin/docker node ls --format "{{.Hostname}}")
-    usednodes=()
-    for node in ${nodes};do
         stackname="$name"-"$node"
         if echo "$stacks" | grep -q "$stackname"; then
-            usednodes+=("$node")
-            ((replicate--))
+            $DOCKER stack deploy -d -c "$FILENAME" "$name"-"$node" 
         fi
     done
+}
 
-    if [ "$replicate" -eq 0 ]; then
+clean_inactive_nodes(){
+    nodes=$(get_nodes | grep -v "Ready Active" | cut -d ' ' -f 1)
+    for node in ${nodes}; do
+        $DOCKER stack rm "$name"-"$node" 2> /dev/null
+    done
+}
+
+deploy_one(){
+    $DOCKER stack deploy -d -c "$FILENAME" "$name"-"$1"
+    serviceID=$($DOCKER stack services "$name"-"$1" -q)
+    $DOCKER service update -d --constraint-add node.hostname=="$1" "$serviceID"
+}
+
+deploy_global(){
+    nodes=$(get_nodes | grep "Ready Active" | sort | cut -d ' ' -f 1)
+    stacks=$(get_stack)
+    if [ "$1" = "--all" ]; then
+        nodes=$(get_nodes | sort | cut -d ' ' -f 1)
+    fi
+    for node in ${nodes}; do
+        stackname="$name"-"$node"
+        if ! echo "$stacks" | grep -q "$stackname"; then
+            deploy_one "$node"
+        fi
+    done
+}
+check_done(){
+    if [ "$1" -eq 0 ]; then
         exit 0
     fi
+}
+
+deploy_n_replicas(){
+    replicate=$1
+    stacks=$(get_stack)
+    nodes=$(get_nodes | cut -d ' ' -f 1)
+    usednodes=""
+    for node in $nodes;do
+        stackname="$name"-"$node"
+        if echo "$stacks" | grep -q "$stackname"; then
+            usednodes="$node $usednodes"
+            replicate=$((replicate - 1))
+        fi
+    done
+
+    check_done "$replicate"
 
     if [ "$replicate" -lt 0 ]; then
-        usednodesrandom=$(echo "${usednodes[@]}" | tr ' ' '\n' | sort -R)
+        usednodesrandom=$(echo "$usednodes" | tr ' ' '\n' | sort -R)
         for node in ${usednodesrandom}; do
-            /usr/bin/docker stack rm -d  "$name"-"$node"
-            ((replicate++))
-            if [ "$replicate" -eq 0 ]; then
-                exit 0
-            fi
+            $DOCKER stack rm "$name"-"$node"
+            replicate=$((replicate + 1))
+            check_done $replicate
         done
     fi
 
-    readynodes=$(/usr/bin/docker node ls --format "{{.Hostname}} {{.Status}} {{.Availability}}" | grep "Ready Active" | sort -R | cut -d ' ' -f 1)
+    readynodes=$(get_nodes | grep "Ready Active" | sort -R | cut -d ' ' -f 1)
     readynodecount=$(echo "$readynodes" | wc -w)
 
     if [ "$readynodecount" -ge 1 ]; then
         for node in ${readynodes};do
-            if echo "${usednodes[@]}" | grep -v -q "$node"; then
-                deployone "$node"
-                ((replicate--))
+            if ! echo "$usednodes" | grep -q "$node"; then
+                deploy_one "$node"
+                replicate=$((replicate - 1))
             fi
-            if [ "$replicate" -eq 0 ]; then
-                exit 0
-            fi
+            check_done "$replicate"
         done
     fi
 }
 
-if [ "$2" == "remove" ]; then
-    removeall
+if [ "$2" = "remove" ]; then
+    remove_all_stacks
 
-elif [ "$2" == "clean" ]; then
-    cleanup
+elif [ "$2" = "clean" ]; then
+    clean_inactive_nodes
 
-elif [ "$2" == "update" ]; then
-    update
+elif [ "$2" = "update" ]; then
+    update_stack
 
-elif [ "$2" == "deploy" ]; then
-    if [ "$3" ]; then
-        deployn "$3"
+elif [ "$2" = "deploy" ]; then
+
+    if [ "$3" = "--all" ]; then
+        deploy_global "$3"
+    elif [ "$3" ]; then
+        deploy_n_replicas "$3"
     else
-        deployglobal
-    fi    
+        deploy_global
+    fi
+
+elif [ "$2" = "help" ]; then
+    echo "Usage:"
+    echo "  ./run file.yml deploy [count]  - Deploy to --all or N nodes"
+    echo "  ./run file.yml update          - Update existing stacks"
+    echo "  ./run file.yml remove          - Remove all stacks"
+    echo "  ./run file.yml clean           - Remove stacks from inactive nodes"
+    exit 0
 fi
