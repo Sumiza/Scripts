7084fd0cb97f32c2388c19d5974c1031c7ffeb92
Sumiza <64884509+Sumiza@users.noreply.github.com>
2025-07-23 15:37:26 -0500

    Update dockerpod.sh
    generated notes added

--- dockerpod.sh
+++ dockerpod.sh
@@ -1,22 +1,26 @@
 #!/bin/sh
 
+# Check for minimum 2 arguments: the compose file and a command
 if [ $# -lt 2 ]; then
     echo "Usage: ./run <compose-file.yml> [remove|clean|update|deploy [replicas|--all]]"
     exit 1
 fi
 
-FILENAME="$1"
-name=$(echo "$FILENAME" | cut -d '.' -f 1)
-DOCKER=$(command -v docker)
+FILENAME="$1"  # First argument: Docker Compose file
+name=$(echo "$FILENAME" | cut -d '.' -f 1)  # Extract stack base name from file
+DOCKER=$(command -v docker)  # Find docker binary location
 
+# Get list of all Docker nodes with their status and availability
 get_nodes(){
     $DOCKER node ls --format "{{.Hostname}} {{.Status}} {{.Availability}}"
 }
 
+# Get list of currently running stack names
 get_stack(){
     $DOCKER stack ls --format "{{.Name}}"
 }
 
+# Remove all stacks matching the naming pattern across all nodes
 remove_all_stacks(){
     nodes=$(get_nodes | cut -d ' ' -f 1)
     for node in ${nodes}; do
@@ -24,6 +28,7 @@ remove_all_stacks(){
     done 
 }
 
+# Re-deploy stacks that already exist on active nodes
 update_stack(){
     stacks=$(get_stack)
     nodes=$(get_nodes | cut -d ' ' -f 1)
@@ -35,6 +40,7 @@ update_stack(){
     done
 }
 
+# Remove stacks on nodes that are not Ready and Active
 clean_inactive_nodes(){
     nodes=$(get_nodes | grep -v "Ready Active" | cut -d ' ' -f 1)
     for node in ${nodes}; do
@@ -42,12 +48,14 @@ clean_inactive_nodes(){
     done
 }
 
+# Deploy stack to a specific node with a constraint
 deploy_one(){
     $DOCKER stack deploy -d -c "$FILENAME" "$name"-"$1"
     serviceID=$($DOCKER stack services "$name"-"$1" -q)
     $DOCKER service update -d --constraint-add node.hostname=="$1" "$serviceID"
 }
 
+# Deploy stack globally to all (or --all) nodes that don't already have it
 deploy_global(){
     nodes=$(get_nodes | grep "Ready Active" | sort | cut -d ' ' -f 1)
     stacks=$(get_stack)
@@ -61,18 +69,23 @@ deploy_global(){
         fi
     done
 }
+
+# Exit early if the target number of replicas is already satisfied
 check_done(){
     if [ "$1" -eq 0 ]; then
         exit 0
     fi
 }
 
+# Deploy to a specific number of nodes
 deploy_n_replicas(){
     replicate=$1
     stacks=$(get_stack)
     nodes=$(get_nodes | cut -d ' ' -f 1)
     usednodes=""
-    for node in $nodes;do
+
+    # Identify nodes already running the stack
+    for node in $nodes; do
         stackname="$name"-"$node"
         if echo "$stacks" | grep -q "$stackname"; then
             usednodes="$node $usednodes"
@@ -82,6 +95,7 @@ deploy_n_replicas(){
 
     check_done "$replicate"
 
+    # If too many replicas exist, randomly remove extras
     if [ "$replicate" -lt 0 ]; then
         usednodesrandom=$(echo "$usednodes" | tr ' ' '\n' | sort -R)
         for node in ${usednodesrandom}; do
@@ -91,11 +105,12 @@ deploy_n_replicas(){
         done
     fi
 
+    # Deploy to additional Ready Active nodes if needed
     readynodes=$(get_nodes | grep "Ready Active" | sort -R | cut -d ' ' -f 1)
     readynodecount=$(echo "$readynodes" | wc -w)
 
     if [ "$readynodecount" -ge 1 ]; then
-        for node in ${readynodes};do
+        for node in ${readynodes}; do
             if ! echo "$usednodes" | grep -q "$node"; then
                 deploy_one "$node"
                 replicate=$((replicate - 1))
@@ -105,6 +120,7 @@ deploy_n_replicas(){
     fi
 }
 
+# Handle subcommands
 if [ "$2" = "remove" ]; then
     remove_all_stacks
 
@@ -115,7 +131,6 @@ elif [ "$2" = "update" ]; then
     update_stack
 
 elif [ "$2" = "deploy" ]; then
-
     if [ "$3" = "--all" ]; then
         deploy_global "$3"
     elif [ "$3" ]; then
