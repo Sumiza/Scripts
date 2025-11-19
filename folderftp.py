6b139e2213041649713cbc35cede1d7fd93a1ea8
Sumiza <64884509+Sumiza@users.noreply.github.com>
2025-11-18 18:54:21 -0600

    Fix local folder path and comment out file retrieval
    Update the path for local folder creation and adjust file retrieval logic.

--- folderftp.py
+++ folderftp.py
@@ -55,13 +55,14 @@ class FolderFtps():
         def recursive_folders(folder:str):
             for file_folder, data in self.ftp.mlsd(folder):
                 file_folder_tree = folder + '/' + file_folder
-                folder_file_abs = os.path.abspath(file_folder_tree)
+                folder_file_abs = os.path.abspath(self.local_folder + file_folder_tree)
                 os.makedirs(os.path.dirname(folder_file_abs),exist_ok=True)
+                print(folder_file_abs)
                 if data['type'] == 'dir':
                     recursive_folders(file_folder_tree)
                 if data['type'] == 'file':
                     with open(folder_file_abs,'wb') as f:
-                        self.ftp.retrbinary(f'RETR {file_folder_tree}',lambda data: f.write(data))
+                        # self.ftp.retrbinary(f'RETR {file_folder_tree}',lambda data: f.write(data))
                         print(f'Wrote file {file_folder}')
         recursive_folders(self.remote_folder)
 
